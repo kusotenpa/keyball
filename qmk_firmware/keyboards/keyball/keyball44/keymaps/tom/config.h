e44ea81fcb4e96dcef0e08366073be9064db0068
tom <t.bass0326@gmail.com>
2026-05-31 13:19:17 +0900

    キーマップとOLED表示の設定を調整

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/tom/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/tom/config.h
@@ -1,23 +1,3 @@
-/*
-This is the c configuration file for the keymap
-
-Copyright 2022 @Yowkees
-Copyright 2022 MURAOKA Taro (aka KoRoN, @kaoriya)
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #pragma once
 
 #ifdef RGBLIGHT_ENABLE
@@ -33,14 +13,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #    define RGBLIGHT_EFFECT_TWINKLE
 #endif
 
+#define TAP_CODE_DELAY 5
+#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
+#define AUTO_MOUSE_DEFAULT_LAYER 2
 
+// custom settings
 #define TAPPING_TERM 0
 #define PERMISSIVE_HOLD
 #define RETRO_TAPPING
-
 #define KEYBALL_AUTO_MOUSE_THRESHOLD 2
-
-#define TAP_CODE_DELAY 5
-
-#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 2
+#define OLED_TIMEOUT 30000
--- qmk_firmware/keyboards/keyball/keyball44/keymaps/tom/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/tom/keymap.c
@@ -23,16 +23,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default
   [0] = LAYOUT_universal(
     KC_TAB,       KC_Q,        KC_W,      KC_E,      KC_R,      KC_T,                                        KC_Y     , KC_U     , KC_I     , KC_O     ,         KC_P     , KC_BSPC ,
-    MT(MOD_LCTL,KC_ESC),     KC_A,      KC_S,      KC_D,      KC_F,      KC_G,                               KC_H     , KC_J ,    KC_K     , KC_L     , KC_SCLN  , KC_ENT  ,
-    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   ,         KC_SLSH  , KC_QUOT  ,
-                         KC_LALT,    LT(3, KC_MINUS),   LGUI_T(KC_LNG2),  KC_SPC,  _______,                  LM(1,MOD_LGUI),  LT(1,KC_LNG1),   _______,   _______,    KC_GRV
+    KC_LCTL,     KC_A,      KC_S,      KC_D,      KC_F,      KC_G,                                           KC_H     , KC_J ,    KC_K     , KC_L     , KC_SCLN  , KC_ENT  ,
+    MT(MOD_LSFT,KC_ESC), KC_Z,         KC_X,      KC_C,     KC_V,   KC_B ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   ,         KC_SLSH  , KC_QUOT  ,
+                 KC_LALT,    LT(3, KC_MINUS),   KC_LGUI,   KC_LNG2,  KC_LNG1,                  LM(1,MOD_LGUI),  LT(1,KC_SPC),   _______,   _______,    KC_GRV
   ),
 
   [1] = LAYOUT_universal(
     _______ ,  KC_EXLM   , KC_AT    , KC_PIPE   , KC_LCBR    , KC_RCBR    ,                                    _______, KC_1    , KC_2    , KC_3    , _______   , _______   ,
-    _______ ,  KC_ASTR , KC_DLR  , KC_AMPR   , KC_LPRN   , KC_RPRN   ,                                         KC_DOT, KC_4  , KC_5     , KC_6   , _______  , KC_ENT   ,
-    _______ ,  KC_HASH , KC_CIRC  , KC_PERC , KC_LBRC  , KC_RBRC  ,                                         KC_0  , KC_7    , KC_8     ,KC_9 , _______  , _______  ,
-                  KC_BSLS  , KC_PPLS , KC_EQL  ,         KC_UP  , KC_DOWN  ,                   _______  , _______  , _______       , _______  , _______
+    KC_LGUI ,  KC_ASTR , KC_DLR  , KC_AMPR   , KC_LPRN   , KC_RPRN   ,                                         KC_0, KC_4  , KC_5     , KC_6   , _______  , KC_ENT   ,
+    _______ ,  KC_HASH , KC_CIRC  , KC_PERC , KC_LBRC  , KC_RBRC  ,                                         KC_DOT  , KC_7    , KC_8     ,KC_9 , _______  , _______  ,
+                         KC_BSLS,   KC_PPLS,  KC_EQL,    KC_UP,     KC_DOWN,                   _______  , _______  , _______       , _______  , _______
   ),
 
   [2] = LAYOUT_universal(
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RGB_TOG  , AML_TO   , AML_I50  , AML_D50  , SSNP_FRE  , SCRL_MO  ,                                        RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
     RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SCRL_DVI ,                                        RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW , _______  , _______  ,
     RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , SCRL_DVD ,                                        CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
-                  QK_BOOT  , KBC_RST  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , KBC_RST  , QK_BOOT
+                  QK_BOOT  , _______  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , KBC_RST  , QK_BOOT
   ),
 };
 
@@ -59,6 +59,30 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 #ifdef OLED_ENABLE
 
 #include "lib/oledkit/oledkit.h"
+#include "custom_oled.c"
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    return OLED_ROTATION_270;
+}
+
+bool oled_task_user(void) {
+  if (last_input_activity_elapsed() > OLED_TIMEOUT) {
+        oled_off();
+        return false;
+    }
+
+    if (!is_oled_on()) {
+        return false;
+    }
+
+    if (is_keyboard_master()) {
+        oledkit_render_info_user();
+    } else {
+         oled_render_custom();
+    }
+
+    return false;
+}
 
 void oledkit_render_info_user(void) {
     keyball_oled_render_keyinfo();
